fd24704a8429f6e33e19853005928a072dc5ad6c
Costin79 <costpopr@protonmail.com>
2022-03-08 11:06:40 -0600

    first

---
+++ Spa/RatingsState.swift
@@ -0,0 +1,17 @@
+//
+//  RatingsState.swift
+//  Spa
+//
+//  Created by costin popescu on 3/8/22.
+//
+
+import Foundation
+
+class RatingsState{
+    static var ratings = [
+                    
+                    "SpaStaff":0,
+                    "Massage":0,
+                    "SpaServices": 0
+                  ]
+}
